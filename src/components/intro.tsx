a54c9ee5fd5a3522c4a6ec5a6ea1e8e18900548b
elianisdev <elianismedina05@outlook.com>
2023-10-28 09:16:20 -0500

    added IG icon

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { AiOutlineYoutube } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
@@ -121,6 +122,13 @@ export default function Intro() {
           target="_blank"
         >
           <AiOutlineYoutube />
+        </a>
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.instagram.com/elianismedina05/"
+          target="_blank"
+        >
+          <AiOutlineInstagram />
         </a>
       </motion.div>
     </section>
