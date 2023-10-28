fb817fdde40fdabd29e6f51a4e41d7d49bba9b4f
elianisdev <elianismedina05@outlook.com>
2023-10-28 13:18:48 -0500

    organized into section

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -25,6 +25,7 @@ export default function Intro() {
     >
       <div className="flex items-center justify-center">
         <div className="relative">
+          {/* IMAGE */}
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -59,7 +60,7 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-
+      {/* INTRODUCTION */}
       <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
@@ -80,6 +81,7 @@ export default function Intro() {
           delay: 0.1,
         }}
       >
+        {/* CONTACT BUTTON */}
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
@@ -91,7 +93,7 @@ export default function Intro() {
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
-
+        {/* CV DOWNLOAD */}
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/CV.pdf"
@@ -100,7 +102,7 @@ export default function Intro() {
           Download CV{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
-
+        {/* LINKEDIN */}
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/elianis-medina/"
@@ -108,7 +110,7 @@ export default function Intro() {
         >
           <BsLinkedin />
         </a>
-
+        {/* GITHUB */}
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://github.com/elianismedina"
@@ -116,6 +118,7 @@ export default function Intro() {
         >
           <FaGithubSquare />
         </a>
+        {/* YOUTUBE */}
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.youtube.com/channel/UCSpa16D5IK-e8N38zw1YrVg"
@@ -123,6 +126,7 @@ export default function Intro() {
         >
           <AiOutlineYoutube />
         </a>
+        {/* INSTAGRAM */}
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://www.instagram.com/elianismedina05/"
