c51a95a368b41df1f6b0f49984aaa90f02bfa210
elianisdev <elianismedina05@outlook.com>
2023-10-28 09:01:30 -0500

    added social media icons

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -8,6 +8,7 @@ import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import { AiOutlineYoutube } from "react-icons/ai";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
@@ -101,7 +102,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/elianis-medina/"
           target="_blank"
         >
           <BsLinkedin />
@@ -109,10 +110,17 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/elianismedina"
           target="_blank"
         >
           <FaGithubSquare />
+        </a>
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.youtube.com/channel/UCSpa16D5IK-e8N38zw1YrVg"
+          target="_blank"
+        >
+          <AiOutlineYoutube />
         </a>
       </motion.div>
     </section>
