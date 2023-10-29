8a4c58c3ccf0383d2b87058d1aa90953682e5008
elianisdev <elianismedina05@outlook.com>
2023-10-29 10:20:01 -0500

    changed intro

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -68,7 +68,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I&rsquo;m Elianis.</span> I&rsquo;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
+        <span className="font-bold">1 year</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
