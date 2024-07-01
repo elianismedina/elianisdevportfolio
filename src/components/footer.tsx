af2ffd388cb00bb75d3772671683c31d5de4097d
elianismedina <elianismedina05@outlook.com>
2024-07-01 11:05:27 -0500

    updated cv

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="px-4 text-center text-gray-50 bg-black p-12 ">
       <small className="mb-2 block text-md">
-        &copy; 2023 ElianisDev. All rights reserved.
+        &copy; 2024 ElianisDev. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
